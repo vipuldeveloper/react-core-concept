2216bccf306a6dffafc94814f0cd2639e83da486
DESKTOP-PMBVERA\vipul <vipul.dholariya@tarento.com>
2018-03-07 23:15:58 +0530

    HelloWorld by 2 different component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Hello extends React.Component {
+    render() {
+        return (
+            <h1>Hello</h1>
+        )
+    }
+}
+
+class World extends React.Component {
+    render() {
+        return (
+            <h1>World</h1>
+        )
+    }
+}
+
 class HelloWorld extends React.Component {
     render() {
         return (
-            <h1>Helloworld</h1>
+            <div>
+                <Hello/>
+                <World/>
+            </div>
         )
     }
 }
